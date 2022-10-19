549edc2d32b84d7ba859acc5f69ed35b272f9e08
jaseemmohammed1987 <jaseemmohammed1987@gmail.com>
2022-10-19 10:35:48 +0530

    add dependency injection

--- UserApi/Program.cs
+++ UserApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using UserApi.Business;
+using UserApi.Repository;
 using UserApi.Repository.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,7 +8,8 @@ builder.Services.AddDbContext<UserApiContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("UserApiContext") ?? throw new InvalidOperationException("Connection string 'UserApiContext' not found.")));
 
 // Add services to the container.
-
+builder.Services.AddTransient<IUserManager, UserManager>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
